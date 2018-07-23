dcb847d3d456fb9b2484f210389fe1416185998b
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2018-07-23 08:44:53 -0700

    Update app.js

--- app.js
+++ app.js
@@ -392,14 +392,12 @@ function decode(html) {
 }
 
 function getFormattedDate(date) {
+	date=new Date(date);
 	var year = date.getFullYear();
-
 	var month = (1 + date.getMonth()).toString();
 	month = month.length > 1 ? month : '0' + month;
-
 	var day = date.getDate().toString();
 	day = day.length > 1 ? day : '0' + day;
-
 	return month + '/' + day + '/' + year;
 }
 
