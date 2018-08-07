9f02d07ec9195a97f7f6a52dafd8025967652e13
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2018-08-07 09:51:13 -0700

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,8 +1,7 @@
 self.importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
 self.importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
 self.addEventListener('push', function(event) {
-    console.log((event.data.json()));
-    var data = event.data;
+    var data = event.data.json();
     var notification = self.registration.showNotification(data.title, {
         body: data.content,
         icon: '/gatherapp/512x512.png'
