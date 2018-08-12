41192a9452fa01ec3d28fc06bd889cdc85783c4a
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2018-08-12 16:45:38 -0700

    Update app.js

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ function feed(){
 			write("Welcome!",[{text:"Welcome to GatherApp, "+name+"!"}]);
 		}
 		notifications.forEach(function(notification){
-			write(notification.val().title,[{text:notification.val().content}],[{text:"Dismiss",href:"clearFeed('"+notification.key+"');"}],"loadGatherUp('"+notification.val().tag+"');");
+			write(notification.val().title,[{text:notification.val().content}],[{text:"Dismiss",href:"clearFeed('"+notification.key+"');event.stopPropagation();"}],"loadGatherUp('"+notification.val().tag+"');");
 		});
 	});
 }
