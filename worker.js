ae4f1f3468a72a58120ed5622bed4c55da45294c
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2018-08-16 13:33:07 -0700

    Update worker.js

--- worker.js
+++ worker.js
@@ -4,7 +4,7 @@ self.addEventListener('push', function(event) {
     var data = event.data.json();
     var notification = self.registration.showNotification(data.title, {
         body: data.content,
-        action:data.action,
+        tag:Date.now()+":"+data.tag,
         icon: '/gatherapp/512x512.png'
     });
 });
@@ -15,12 +15,12 @@ self.addEventListener('notificationclick', function(event) {
             var client = clientList[i];
             if ('focus' in client){
                 return client.focus().then(function(){
-                    client.navigate('https://kentonishi.github.io/gatherapp/#'+event.notification.action);
+                    client.navigate('https://kentonishi.github.io/gatherapp/#'+event.notification.tag.split(":")[1]);
                 });
             }
         }
         if (clients.openWindow){
-            return clients.openWindow('https://kentonishi.github.io/gatherapp/#'+event.notification.action);
+            return clients.openWindow('https://kentonishi.github.io/gatherapp/#'+event.notification.tag.split(":")[1]);
         }
     }));
 });
