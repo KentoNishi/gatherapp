3e78370a984f7c96b7ce8652b70dcf18058424f5
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2018-08-17 13:03:51 -0700

    Update app.js

--- app.js
+++ app.js
@@ -150,15 +150,6 @@ function fillInAddress() {
 		contents.push({html:"</div>"});
 		contents.push({html:"<button onclick='"+"newBoardPost('"+id+"');"+"'>Publish</button>"});
 		write("New Post",contents,[{href:"loadGatherUp('"+id+"');",text:"Cancel"}]);
-		autocomplete = new google.maps.places.Autocomplete((document.querySelectorAll(".inputs")[0].querySelectorAll("input")[1]),{ fields: ["name", "place_id", "formatted_address"] });
-		google.maps.event.addListener(autocomplete, 'place_changed', function () {
-			if(autocomplete.getPlace().formatted_address.split(",").length>3){
-				document.querySelectorAll(".inputs")[0].querySelectorAll(".iframe")[0].style.display="block";
-				document.querySelectorAll(".inputs")[0].querySelectorAll("iframe")[0].src="https://www.google.com/maps/embed/v1/place?q=place_id:"+autocomplete.getPlace().place_id+"&key=AIzaSyAiOBh4lWvseAsdgiTCld1WMXEMVo259hM";
-			}else{
-				alert("You must choose a valid location.");
-			}
-		});
 }
 
 function newBoardPost(id){
