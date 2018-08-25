8b181d610d1f48095f384b327f3037607a0a0755
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2018-08-25 11:28:02 -0700

    Update app.js

--- app.js
+++ app.js
@@ -330,11 +330,10 @@ function loadGatherUps(){
 						addr=gatherup.val().location.name+","+gatherup.val().location.formatted_address.split(",").slice(1,gatherup.val().location.formatted_address.split(",").length).join(",");
 					}
 					var contents=[{text:(gatherup.val().date==null?"Unknown Date":date)},{text:addr!=null?addr.split(",").slice(0,addr.split(",").length-2).join(","):"Unknown Location"}];
+					contents.push({text:gatherup.val().duration!=null?(Math.floor(gatherup.val().duration/60)+"h"+(gatherup.val().duration%60)+"m Long"):"Unknown Duration"});
 					if(gatherup.val().date!=null&&new Date(gatherup.val().date).getTime()<new Date().getTime()){
 						contents.push({html:"<span style='color:red;font-size:4vh;'>Ongoing Event</span>"});
 					}
-					var gather=gatherup;
-					contents.push({text:gather.val().duration!=null?(Math.floor(gather.val().duration/60)+"h"+(gather.val().duration%60)+"m Long"):"Unknown Duration"});
 					write(gatherup.val().title,contents,null,"loadGatherUp('"+gather.key+"');");
 				}
 			});
