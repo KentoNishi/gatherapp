69c7f7095e99d63fdadc93cd49fb18ae3c8e9663
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2018-08-28 22:16:10 -0700

    Update temp.js

--- temp.js
+++ temp.js
@@ -10,7 +10,7 @@ firebase.database().ref("gatherups/"+id+"/board/").on("value",posts=>{
 		var contents=[];
 		reverse(posts).forEach(post=>{
 			firebase.database().ref("users/"+post.val().author+"/info").once("value",author=>{
-				contents.push("<div style='background-color:"+(post.val().author==uid?"cornflowerblue":"orange")+";border-radius:2vh;padding:1vh;margin:0;width:fit-content;'>"+encode(post.val().content)+"<div style='text-align:left;'>"+encode(author.val().name)+"</div></div>");
+				contents.push("<div style='background-color:"+(post.val().author==uid?"cornflowerblue":"orange")+";border-radius:2vh;padding:1vh;margin:0;width:fit-content;'>"+encode(post.val().content)+"<div style='text-align:center;'>"+encode(author.val().name)+"</div></div>");
 				if(post.key==Object.keys(posts.val())[Object.keys(posts.val()).length-1]){
 					write("Event Board",[{html:"<div class='board' style='text-align:left;max-height:60vh;overflow-y:auto;min-width:75vw;background-color:white;'>"+contents.join("<br />")+"</div>"}]);
 					document.querySelectorAll(".board")[0].scrollTop=document.querySelectorAll(".board")[0].scrollHeight;
