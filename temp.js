bda42a231b27f1533983c1690362b444ba67084e
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2018-08-28 21:59:28 -0700

    Update temp.js

--- temp.js
+++ temp.js
@@ -1,22 +1,20 @@
 firebase.database().ref("gatherups/-LKn7-fdh8rX_JRNO62C/board/info").push().update({
 
-		content:"content",
+		content:"content content content content content content content content ",
 		author:uid
 
 });
 clear();
 firebase.database().ref("gatherups/-LKn7-fdh8rX_JRNO62C/board/info").once("value",posts=>{
-	var contents=[];console.log(posts.val());
-	posts.forEach(post=>{console.log(post.val());
+	var contents=[];
+	reverse(posts).forEach(post=>{
 		firebase.database().ref("users/"+post.val().author+"/info").once("value",author=>{
-			contents.push({text:post.val().content});
-			contents.push({text:author.val().name});
-            if(post.key==Object.keys(posts.val())[0]){
-                write("Event Board",contents);
+			contents.push("<div style='background-color:orange;border-radius:2vh;padding:1vh;margin:0;width:fit-content;'>"+encode(post.val().content)+" -"+encode(author.val().name)+"</div>");
+            if(post.key==Object.keys(posts.val())[Object.keys(posts.val()).length-1]){
+                write("Event Board",[{html:"<div class='board' style='text-align:left;max-height:60vh;overflow-y:auto;min-width:75vw;background-color:white;'>"+contents.join("<br />")+"</div>"}]);
+document.querySelectorAll(".board")[0].scrollTop=document.querySelectorAll(".board")[0].scrollHeight;
             }else{
-				contents.push({text:""});
             }
         });
     });
 });
-
