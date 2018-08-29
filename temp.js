b358f6f6cd9e9c79a0030ad0b0ff6beef05e3178
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2018-08-28 22:09:09 -0700

    Update temp.js

--- temp.js
+++ temp.js
@@ -1,20 +1,21 @@
-firebase.database().ref("gatherups/-LKn7-fdh8rX_JRNO62C/board/info").push().update({
-
-		content:"content content content content content content content content ",
-		author:uid
-
+firebase.database().ref("gatherups/"+id+"/board/").push().update({
+	content:"content content content content content content content content ",
+	author:uid
 });
-clear();
-firebase.database().ref("gatherups/-LKn7-fdh8rX_JRNO62C/board/info").once("value",posts=>{
-	var contents=[];
-	reverse(posts).forEach(post=>{
-		firebase.database().ref("users/"+post.val().author+"/info").once("value",author=>{
-			contents.push("<div style='background-color:orange;border-radius:2vh;padding:1vh;margin:0;width:fit-content;'>"+encode(post.val().content)+" -"+encode(author.val().name)+"</div>");
-            if(post.key==Object.keys(posts.val())[Object.keys(posts.val()).length-1]){
-                write("Event Board",[{html:"<div class='board' style='text-align:left;max-height:60vh;overflow-y:auto;min-width:75vw;background-color:white;'>"+contents.join("<br />")+"</div>"}]);
-document.querySelectorAll(".board")[0].scrollTop=document.querySelectorAll(".board")[0].scrollHeight;
-            }else{
-            }
-        });
-    });
+var onced=false;
+firebase.database().ref("gatherups/"+id+"/board/").on("value",posts=>{
+	if(!onced||(document.querySelectorAll(".board").length>0&&posts.val()[Object.keys(posts.val())[Object.keys(posts.val()).length-1]].content!=null&&posts.val()[Object.keys(posts.val())[Object.keys(posts.val()).length-1]].title!=null)){
+		clear();
+		var contents=[];
+		reverse(posts).forEach(post=>{
+			firebase.database().ref("users/"+post.val().author+"/info").once("value",author=>{
+				contents.push("<div style='background-color:orange;border-radius:2vh;padding:1vh;margin:0;width:fit-content;'>"+encode(post.val().content)+" -"+encode(author.val().name)+"</div>");
+				if(post.key==Object.keys(posts.val())[Object.keys(posts.val()).length-1]){
+					write("Event Board",[{html:"<div class='board' style='text-align:left;max-height:60vh;overflow-y:auto;min-width:75vw;background-color:white;'>"+contents.join("<br />")+"</div>"}]);
+					document.querySelectorAll(".board")[0].scrollTop=document.querySelectorAll(".board")[0].scrollHeight;
+				}else{
+				}
+			});
+		});
+	}
 });
