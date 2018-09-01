9882b4a6f38b75978d6ec91a62c2840a451162a4
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2018-09-01 14:48:34 -0700

    Update app.js

--- app.js
+++ app.js
@@ -345,7 +345,8 @@ function loadEventBoard(id,callback){
 				if(document.querySelectorAll(".board").length>0){
 					document.querySelectorAll(".board")[0].innerHTML="<br />"+contents.join("<br />")+"<br />";
 				}else{
-					write("Event Board",[{html:"<div class='board' style='text-align:center;height:50vh;overflow-y:auto;min-width:75vw;background-color:white;'><br />"+contents.join("<br />")+"<br /></div><textarea placeholder='Type A Message...' oninput='autogrow(this);' style='overflow:hidden;resize:none;margin-top:2.5vh;margin-bottom:2.5vh;height:5vh;max-width:75vw;min-width:75vw;'></textarea><br /><button onclick='newBoardPost("+'"'+id+'"'+");' class='post' style='margin-bottom:1.5vh;'>Post To Board</button>"}]);
+					write("Event Board",[{html:"<div class='board' style='text-align:center;height:50vh;overflow-y:auto;min-width:75vw;background-color:white;'><br />"+contents.join("<br />")+"<br /></div><textarea placeholder='Type A Message...' oninput='autogrow(this);' style='overflow:hidden;resize:none;margin-top:2.5vh;margin-bottom:2.5vh;height:5vh;max-width:75vw;min-width:75vw;'></textarea><br /><button onclick='newBoardPost("+'"'+id+'"'+");' style='margin-bottom:1.5vh;'>Post To Board</button>"}]);
+					document.querySelectorAll(".body")[0].innerHTML+="<span class='post'></span>";
 				}
 				}else{
 				contents=[];
@@ -363,7 +364,8 @@ function loadEventBoard(id,callback){
 					if(document.querySelectorAll(".board").length>0){
 						document.querySelectorAll(".board")[0].innerHTML="<br />"+contents.join("<br />")+"<br />";
 					}else{
-						write("Event Board",[{html:"<div class='board' style='text-align:center;height:50vh;overflow-y:auto;min-width:75vw;background-color:white;'><br />"+contents.join("<br />")+"<br /></div><textarea placeholder='Type A Message...' oninput='autogrow(this);' style='overflow:hidden;resize:none;margin-top:2.5vh;margin-bottom:2.5vh;height:5vh;max-width:75vw;min-width:75vw;'></textarea><br /><button onclick='newBoardPost("+'"'+id+'"'+");' class='post' style='margin-bottom:1.5vh;'>Post To Board</button>"}]);
+						write("Event Board",[{html:"<div class='board' style='text-align:center;height:50vh;overflow-y:auto;min-width:75vw;background-color:white;'><br />"+contents.join("<br />")+"<br /></div><textarea placeholder='Type A Message...' oninput='autogrow(this);' style='overflow:hidden;resize:none;margin-top:2.5vh;margin-bottom:2.5vh;height:5vh;max-width:75vw;min-width:75vw;'></textarea><br /><button onclick='newBoardPost("+'"'+id+'"'+");' style='margin-bottom:1.5vh;'>Post To Board</button>"}]);
+						document.querySelectorAll(".body")[0].innerHTML+="<span class='post'></span>";
 					}
 					document.querySelectorAll(".board")[0].scrollTop=document.querySelectorAll(".board")[0].scrollHeight*innerHeight;
 				}else{
