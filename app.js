2f124751a97a80859a287dde24d4b32f1a40bb6f
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2018-09-01 17:48:06 -0700

    Update app.js

--- app.js
+++ app.js
@@ -210,6 +210,9 @@ function loadGatherUp(id,newuser,callback){
 				}else{
 					member=me.val();
 				}
+				firebase.database().ref("users/"+uid+"/gatherups").update({
+					id:1
+				});
 				var link=[{text:"Leave Event",href:"if(confirm('Are you sure you want to leave this event?')){leaveGatherUp('"+id+"');}"}];
 				if(member==null){
 					link=[{text:"Join Event",href:"joinGatherUp('"+id+"');"}];
