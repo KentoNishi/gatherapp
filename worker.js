56771061c26da934c736745fc0988133b24136ec
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2018-09-15 20:21:46 -0700

    Update worker.js

--- worker.js
+++ worker.js
@@ -69,7 +69,7 @@ self.onmessage=function(e){
 
 var CACHE_NAME = "CACHE";
 
-var urlsToCache = [
+var CACHED_URLS = [
     '/gatherapp/app.js',
     '/gatherapp/',
     '/gatherapp/index.html',
@@ -81,57 +81,39 @@ var urlsToCache = [
     '/gatherapp/google.png',
     '/gatherapp/logo.png'
 ];
-const PRECACHE = CACHE_NAME;
-const RUNTIME = 'runtime';
 
-// A list of local resources we always want to be cached.
-const PRECACHE_URLS = urlsToCache;
-
-// The install handler takes care of precaching the resources we always need.
-self.addEventListener('install', event => {
+self.addEventListener("install", function(event) {
   event.waitUntil(
-    caches.open(PRECACHE)
-      .then(cache => cache.addAll(PRECACHE_URLS))
-      .then(self.skipWaiting())
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(CACHED_URLS);
+    })
   );
 });
 
-// The activate handler takes care of cleaning up old caches.
+self.addEventListener("fetch", function(event) {
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request).then(function(response) {
+        if (response) {
+          return response;
+        } else if (event.request.headers.get("accept").includes("text/html")) {
+          return caches.match("/index-offline.html");
+        }
+      });
+    })
+  );
+});
+
 self.addEventListener("activate", function(event) {
   event.waitUntil(
     caches.keys().then(function(cacheNames) {
       return Promise.all(
         cacheNames.map(function(cacheName) {
-          if (CACHE_NAME !== cacheName &&  cacheName.startsWith("CACHE")) {
+          if (CACHE_NAME !== cacheName &&  cacheName.startsWith("gih-cache")) {
             return caches.delete(cacheName);
           }
         })
       );
     })
   );
-});
-
-// The fetch handler serves responses for same-origin resources from a cache.
-// If no response is found, it populates the runtime cache with the response
-// from the network before returning it to the page.
-self.addEventListener('fetch', event => {
-  // Skip cross-origin requests, like those for Google Analytics.
-  if (event.request.url.startsWith(self.location.origin)) {
-    event.respondWith(
-      caches.match(event.request).then(cachedResponse => {
-        if (cachedResponse) {
-          return cachedResponse;
-        }
-
-        return caches.open(RUNTIME).then(cache => {
-          return fetch(event.request).then(response => {
-            // Put a copy of the response in the runtime cache.
-            return cache.put(event.request, response.clone()).then(() => {
-              return response;
-            });
-          });
-        });
-      })
-    );
-  }
 });
