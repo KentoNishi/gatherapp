f1e38294c200280b7106c4943633652379f5e0b8
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2018-09-21 20:29:24 -0700

    Update app.js

--- app.js
+++ app.js
@@ -212,9 +212,7 @@ function loadEventPage(id){
 	changeOns();
 //	back.add("loadEvent('"+id+"');");
 	var firstload=true;
-	console.log(id);
 	ons.push("events/"+id+"/info");
-	console.log(firstload,document.querySelectorAll(".infocard"+id).length>0);
 	firebase.database().ref("events/"+id+"/info").on("value",function(event){
 		if(firstload||document.querySelectorAll(".infocard"+id).length>0){
 			if(firstload){
