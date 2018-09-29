332d005a522a27fa139fb5574b2eb58dbb46cbaa
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2018-09-28 20:52:50 -0700

    Update app.js

--- app.js
+++ app.js
@@ -126,8 +126,7 @@ function requestEvent(id,title,loc,date,place,duration,cancel){
 	var contents=[];
 	var extra="";
 	contents.push({html:""+extra+"<div class='inputs'><input maxlength='50' placeholder='Title'></input>"});
-	contents.push({html:"<input maxlength='50' placeholder='Address/Location' onfocus='this.setSelectionRange(0, this.value.length);"+
-		       'clearAutocomplete(0);'+"'></input>"});
+	contents.push({html:"<input maxlength='50' placeholder='Address/Location' onfocus='this.setSelectionRange(0, this.value.length);"+"'></input>"});
 	contents.push({html:"<input type='datetime-local' onchange='showDate();'></input>"+
 		       "<strong><span style='font-size:4vh;color:#2e73f7;' class='showdate'></span></strong>"});
 	contents.push({html:"<input style='width:10vh;text-align:center;' type='number' min='0' "+
