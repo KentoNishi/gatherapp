adeb59cd8568ec003d4ba61c2d0585581fc6e574
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2018-10-04 13:58:11 -0700

    Update app.js

--- app.js
+++ app.js
@@ -62,6 +62,16 @@ function menu(){
 	}
 }
 
+function linkify(inputText) {
+    var replacePattern1 = /(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
+    var replacedText = inputText.replace(replacePattern1, '<a href="$1" target="_blank">$1</a>');
+    var replacePattern2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
+    var replacedText = replacedText.replace(replacePattern2, '$1<a href="http://$2" target="_blank">$2</a>');
+    var replacePattern3 = /(\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,6})/gim;
+    var replacedText = replacedText.replace(replacePattern3, '<a href="mailto:$1">$1</a>');
+    return replacedText;
+}
+
 function installApp(e){
 	if(e==null){
 		install.prompt();
@@ -1098,7 +1108,7 @@ function write(title,contents,links,href,classlist,overwrite){
 function encode(e){
 	var txt = document.createElement("textarea");
 	txt.innerText = e;
-	return txt.innerHTML;
+	return linkify(txt.innerHTML);
 }
 
 function decode(html) {
