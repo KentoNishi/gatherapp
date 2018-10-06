2401ed8d6732af00f565d48bad24a755bfc0a9fe
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2018-10-05 22:09:52 -0700

    Update app.js

--- app.js
+++ app.js
@@ -219,7 +219,7 @@ function requestEvent(id,title,loc,date,place,duration,cancel){
 	}
 	autocomplete = new google.maps.places.Autocomplete(
 		(document.querySelectorAll(".inputs")[0].querySelectorAll("input")[1]),
-		{ fields: ["name", "place_id", "formatted_address"/*,"address_components"*/] });
+		{ fields: ["name", "place_id", "formatted_address"/*,"address_components",types: ['(cities)']*/] });
 	google.maps.event.addListener(autocomplete, 'place_changed', function(){
 		placeChanged();
 	});
