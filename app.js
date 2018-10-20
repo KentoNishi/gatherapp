d90d536fe1705e4a900a3ea979793b953713af52
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2018-10-19 22:44:05 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1249,7 +1249,7 @@ function iOSPermission(e,a){
 
 function iOSSubscribe(e){
 	if(e!=null&&e!=""){
-		firebase.database().ref("users/"+uid+"/subs/").update({"apns":e}).then(function(){
+		firebase.database().ref("users/"+uid+"/subs/").push({apns:e}).then(function(){
 			if(document.querySelectorAll(".enableButton").length>0){
 				document.querySelectorAll(".enableButton")[0].innerHTML="";
 			}
