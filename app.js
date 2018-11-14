b4106493cf09ce263a523e82b85ceb12f8b6f686
Kento Nishi <38841491+KentoNishi@users.noreply.github.com>
2018-11-13 18:00:21 -0800

    Update app.js

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ function loadInfo(e){
 	}else{
 		clear();
 		write("Terms of Use", null,null, "termsOfUse();");
-		write("GatherApp", [{text:"By Kento Nishi"},[{href:"https://github.com/KentoNishi/gatherapp/",text:"View on GitHub"}]]);
+		write("GatherApp", [{text:"By Kento Nishi"}],[{href:"window.location='https://github.com/KentoNishi/gatherapp/';",text:"View on GitHub"}]);
 	}
 }
 
